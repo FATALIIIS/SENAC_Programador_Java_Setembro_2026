f2b86dbd2065dcc6895e2236b94a733649ebc823
João Pedro Jensen Farias <jp.123pedrofarias@gmail.com>
2026-09-11 17:00:02 -0300

    Aula 11.09.2026

---
+++ Aula 11.09.2026/aula-web/src/test/java/com/joao/aulaweb/AulaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.aulaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

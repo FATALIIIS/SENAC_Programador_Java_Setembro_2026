38bba93f89d10a7844445ae7bf9b3c94f726efed
João Pedro Jensen Farias <jp.123pedrofarias@gmail.com>
2026-09-17 17:22:19 -0300

    Aula 17.09.2026

---
+++ Aula 17.09.2026/HTML_Java_Aulas_Web/src/main/java/com/joao/html_java_aulas_web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.joao.html_java_aulas_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
